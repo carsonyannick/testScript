f4bfd1bdc7603a31556422e240f4c14c9bddcb45
aa <aa@localhost.localdomain>
2017-11-03 12:45:10 +0000

    added and tested deleteRandom()

--- indexTest.sh
+++ indexTest.sh
@@ -4,6 +4,9 @@ numRE='^[0-9]+$'
 # sockets
 mapCppSocket="/tmp/cppMap-socket"
 
+# size of integer used to idenfitfy each name
+idSize=6;
+
 inputFile=$1
 seed=$2
 validationFrequency=$3
@@ -72,7 +75,11 @@ function checkSeed
     echo $1
 }
 
-idSize=6;
+################### check all input parameters ################### 
+checkInputFile $inputFile
+seed=$(checkSeed $seed)
+validationFrequency=$(checkValidationFrequency $validationFrequency)
+initalSize=$(checkInitalSize $initalSize)
 
 function hashh
 {
@@ -100,14 +107,16 @@ function dataLength
 function add
 {
     local output="add   |"
-    # output="$output$(hashh $1)|${#1}$1"
     output="$output$(hashh "$1")|$(dataLength "$1")$1"
     echo "$output"
 }
 
-# echo $(add 'Vil Mlakar')
-# echo $(add 123456789)
-# exit 1
+function delete
+{
+    local output="delete|"
+    output="$output$(hashh "$1")|$(dataLength "$1")$1"
+    echo "$output"
+}
 
 function hashSeed
 {
@@ -116,7 +125,7 @@ function hashSeed
     # echo "seed after $seed"
 }
 
-function getRandomIndex
+function getRandomIndexInMasterArray
 {
     local seedInDec=$(echo $seed | sed 's/.*\([0-9a-f]\{13\}\)$/\1/')
     seedInDec=$(printf "%d" 0x$seedInDec)
@@ -125,11 +134,28 @@ function getRandomIndex
     echo $index
 }
 
-function getElementAtIndex()
+function getRandomIndexInAddedArray
+{
+    local seedInDec=$(echo $seed | sed 's/.*\([0-9a-f]\{13\}\)$/\1/')
+    seedInDec=$(printf "%d" 0x$seedInDec)
+    local arraySize=${#added[@]}
+    local index=$((seedInDec%arraySize))
+    echo $index
+}
+
+function getElementAtIndexInMasterArray()
 {
     local element=${masterNameArray[$1]}
     local onePast=$(($1+1))
     masterNameArray=( "${masterNameArray[@]:0:$1}" "${masterNameArray[@]:$onePast}" )
+    eval $2='$element'
+}
+
+function getElementAtIndexInAddedArray()
+{
+    local element=${added[$1]}
+    local onePast=$(($1+1))
+    added=( "${added[@]:0:$1}" "${added[@]:$onePast}" )
     eval $2='$element'
 }
 
@@ -140,13 +166,39 @@ function sendd
     echo $output
 }
 
+function checkInital
+{
+    local Hash=$(hashh "$1")
+    local test=${initalNames[$Hash]}
+    if ! [ -z "$test" ]; then
+        if [ "$test" != "$1" ]; then
+            echo "Something has gone serously wrong.." $test " " $1
+            exit 4
+        fi
+        unset initalNames["$Hash"]
+    fi
+}
+
 function addRandom
 {
-    # hashSeed
-    local index=$(getRandomIndex)
-    getElementAtIndex $index name
+    local index=$(getRandomIndexInMasterArray)
+    getElementAtIndexInMasterArray $index name
     local output=$(add "$name")
     output=$(sendd "$output")
+    added+=("$name")
+    eval $1='$name'
+}
+
+function deleteRandom
+{
+    local index=$(getRandomIndexInAddedArray)
+    echo "inded: " $index
+    # echo "added Array size before: " ${#added}
+    getElementAtIndexInAddedArray $index name
+    # echo "added Array size after: " ${#added}
+    local output=$(delete "$name")
+    output=$(sendd "$output")
+    checkInital "$name"
     eval $1='$name'
 }
 
@@ -157,12 +209,12 @@ function getAction
     action=$( echo "obase=2; ibase=16; $action" | bc )
     action=$(echo $action | sed 's/.*\([0-1]\{2\}\)$/\1/')
 
-    if [ "$action" == "0" ]; then
+    if [ "$action" == "0" ] || [ "$action" == "00" ]; then
         echo "add"
         return
     fi
 
-    if [ "$action" == "1" ]; then
+    if [ "$action" == "1" ] || [ "$action" == "01" ]; then
         echo "search_existing"
         return
     fi
@@ -180,16 +232,16 @@ function getAction
     echo $action
 }
 
-checkInputFile $inputFile
-seed=$(checkSeed $seed)
-validationFrequency=$(checkValidationFrequency $validationFrequency)
-initalSize=$(checkInitalSize $initalSize)
-
 echo "seed " $seed
 echo "validation Frequency " $validationFrequency
 echo "initalSize " $initalSize 
 
 mapfile -t masterNameArray < $inputFile
+
+if [ $initalSize -gt ${#masterNameArray[@]} ]; then
+    echo "inital size ($initalSize) is more than number of Names supplied (${#masterNameArray[@]})"
+    exit 5
+fi
 
 # output=$(add "2342342")
 # echo "output"  "$output"
@@ -254,10 +306,10 @@ mapfile -t masterNameArray < $inputFile
 # rust implementaiotn 
 # /home/aa/rust/b-tree/target/debug/btree &
 
-# TODO check if initalSize is !> masterNameArray size
 arraySize=${#masterNameArray[@]}
 
 declare -A initalNames
+added=()
 
 for ((i=0;i<initalSize;++i))
 do
@@ -265,19 +317,42 @@ do
     echo "done adding " $name
     initalNames[$(hashh $name)]=$name
     echo "Name in initalNames[$(hashh $name)] = "${initalNames[$(hashh $name)]}
+    # added+=('$name')
+    # echo "Added[0] " ${added[0]}
 done
 
 echo " masterNameArray size " ${#masterNameArray[@]}
 echo " initalNames size " ${#initalNames[@]}
 
+count=0
 while [ ${#masterNameArray[@]} -ne 0 ] && [ ${#initalNames[@]} -ne 0 ]
 do
     hashSeed
     action=$(getAction)
 
     if [ "$action" == "add" ]; then
-        addRandom
+        echo "inside add"
+        # addRandom
     fi
-    echo "inside while"
 
+    if [ "$action" == "search_existing" ]; then
+        echo "inside search_existing"
+        # addRandom
+    fi
+
+    if [ "$action" == "search_non-existing" ]; then
+        echo "inside search_non-existing"
+        # addRandom
+    fi
+
+    if [ "$action" == "delete" ]; then
+        echo "inside delete"
+        # addRandom
+    fi
+
+    $count=$[$count+1]
+    if [ $count -eq $validationFrequency ]; then
+        count=0
+        # compare()
+    fi
 done
